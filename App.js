6074f8022a3522860d9aacb97012430ae33c289c
actjacob <yakupaktar99@gmail.com>
2024-08-06 15:25:42 +0300

    screens category added

--- App.js
+++ App.js
@@ -2,15 +2,33 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./Storage/store";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Category from "./Screens/Category";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
+  const Drawer = createDrawerNavigator();
+  const Stack = createNativeStackNavigator();
+
+  function CategoryOverview() {
+    return (
+      <Drawer.Navigator initialRouteName="Category">
+        <Drawer.Screen name="Kategoriler" component={Category}></Drawer.Screen>
+      </Drawer.Navigator>
+    );
+  }
   return (
     <>
       <Provider store={store}>
-        <View style={styles.container}>
-          <Text>Hello My Galaxy</Text>
-          <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="CategoryOverview"
+              component={CategoryOverview}
+            ></Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     </>
   );
---
+++ Screens/Category.js
@@ -0,0 +1,11 @@
+import { Text, View, StyleSheet } from "react-native";
+
+function Category() {
+  return (
+    <View>
+      <Text>Categories</Text>
+    </View>
+  );
+}
+
+export default Category;
