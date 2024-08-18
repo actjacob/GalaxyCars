d47ee5338a8b3361db0e98208144ff6198f0c1ba
actjacob <yakupaktar99@gmail.com>
2024-08-18 16:02:18 +0300

    Editing Pressable Operations and Redirecting id On Stack Page

--- Screens/Category/CategoryManage.js
+++ Screens/Category/CategoryManage.js
@@ -1,4 +1,4 @@
-import { Pressable, Text, View, StyleSheet, FlatList } from "react-native";
+import { Pressable, Text, View, StyleSheet, FlatList, Button } from "react-native";
 import { Card, Title, Paragraph } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 import { useGetAllCategoryQuery } from "../../Apis/categoryApi";
@@ -7,6 +7,7 @@ import { useState } from "react";
 function CategoryManage() {
   const { data, isloading } = useGetAllCategoryQuery();
   const { category, setCategory } = useState();
+  const Navigation = useNavigation();
   if (isloading) {
     return (
       <View>
@@ -15,21 +16,30 @@ function CategoryManage() {
     );
   }
 
+  const handleCategoryClick = (categoryId) => {
+    console.log("categoryId");
+    console.log(categoryId);
+    Navigation.navigate("CategoryAddOrUpdate", { categoryId: categoryId });
+  };
+
   return (
-    <FlatList
-      data={data}
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({ item }) => (
-        <Pressable>
-          <Card style={styles.card}>
-            <Card.Content>
-              <Title>{item.categoryName} </Title>
-              <Paragraph>Description : {item.categoryDescription} </Paragraph>
-            </Card.Content>
-          </Card>
-        </Pressable>
-      )}
-    ></FlatList>
+    <View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => handleCategoryClick(item.id)}>
+            <Card style={styles.card}>
+              <Card.Content>
+                <Title>{item.categoryName} </Title>
+                <Paragraph>Description : {item.categoryDescription} </Paragraph>
+              </Card.Content>
+            </Card>
+          </Pressable>
+        )}
+      ></FlatList>
+      <Button title="Create Category" onPress={() => handleCategoryClick()}></Button>
+    </View>
   );
 }
 
